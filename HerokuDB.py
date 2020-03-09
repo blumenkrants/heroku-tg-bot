3cb3d35086790a945b711c755e264d3b8aa6e701
Дмитрий Блюменкранц <dmitriy@MacBook-Pro-Dmitrij.local>
2020-03-09 22:41:56 +0300

    new

--- HerokuDB.py
+++ HerokuDB.py
@@ -1,10 +1,16 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.environ['postgres://sepdreekypiqhd:ac8d1d611f3d504f7c312d5b62c0e8d0319925773b62f747da1a3320ee0b3ee4@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/d1d4qe3vqt296e']
+# DATABASE_URL = os.environ['postgres://sepdreekypiqhd:ac8d1d611f3d504f7c312d5b62c0e8d0319925773b62f747da1a3320ee0b3ee4@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/d1d4qe3vqt296e']
+#
+# conn = psycopg2.connect(DATABASE_URL,
+#                         sslmode='require')
 
-conn = psycopg2.connect(DATABASE_URL,
-                        sslmode='require')
+conn = psycopg2.connect(host='ec2-54-246-89-234.eu-west-1.compute.amazonaws.com',
+                        database='d1d4qe3vqt296e',
+                        user='sepdreekypiqhd',
+                        password='ac8d1d611f3d504f7c312d5b62c0e8d0319925773b62f747da1a3320ee0b3ee4')
+
 
 cursor = conn.cursor()
 
@@ -13,4 +19,10 @@ cursor.execute(sql)
 data_base = cursor.fetchall()
 print(data_base)
 
-heroku trusted-ips:add 217.171.159.242 --space acme-prod
+
+# SELECT * FROM INFORMATION_SCHEMA.TABLES
+#
+# \dt *.*
+#
+# как сменить используемую схему в постгре
+
--- bot_diplom.py
+++ bot_diplom.py
@@ -34,10 +34,10 @@ FIRST, SECOND, THIRD, FOURTH, FIVE = range(5)
 #                                user='j949396',
 #                                password='qwerty')
 
-DATABASE_URL = os.environ['postgres://sepdreekypiqhd:ac8d1d611f3d504f7c312d5b62c0e8d0319925773b62f747da1a3320ee0b3ee4@ec2-54-246-89-234.eu-west-1.compute.amazonaws.com:5432/d1d4qe3vqt296e']
-conn = psycopg2.connect(DATABASE_URL,
-                        sslmode='require')
-
+conn = psycopg2.connect(host='ec2-54-246-89-234.eu-west-1.compute.amazonaws.com',
+                        database='d1d4qe3vqt296e',
+                        user='sepdreekypiqhd',
+                        password='ac8d1d611f3d504f7c312d5b62c0e8d0319925773b62f747da1a3320ee0b3ee4')
 
 # Команды для переезда на Постгре
 # pgloader mysql://j949396:qwerty@mysql.j949396.myjino.ru/j949396_blondi-service postgresql:///blondie
